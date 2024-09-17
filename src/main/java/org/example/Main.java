4c8acd402776eb668906e8b62d3ae2168c61df11
cansucebesoy <cansucebesoy3@gmail.com>
2024-09-17 19:09:20 +0300

    init s13d2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,45 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    public static boolean isPalindrome(int num) {
+        num = Math.abs(num);
+        String numStr = String.valueOf(num);
+        String reversedStr = new StringBuilder(numStr).reverse().toString();
+        return numStr.equals(reversedStr);
+    }
+
+
+    public static boolean isPerfectNumber(int num) {
+        if (num < 0) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == num;
+    }
+
+    public static String numberToWords(int num) {
+        if (num < 0) {
+            return "Invalid Value";
+        }
+        StringBuilder result = new StringBuilder();
+        String strNum = String.valueOf(num);
+        String[] names = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+
+        for (int i = 0; i < strNum.length(); i++) {
+            char digit = strNum.charAt(i);
+            int digitIndex = Character.getNumericValue(digit);
+            result.append(names[digitIndex]).append(" ");
+        }
+        return result.toString().trim();
+
+
+    }
 }
